6695c64c4b544b18baea74b96bc0de7e712bf205
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-15 15:08:11 +0300

    done

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,9 +1,3 @@
 def plus_two(num):
     num += 2
     return num
-
-def test_plus_two_with_debugging(capsys):
-    result = None
-    with capsys.disabled():
-        result = plus_two(5)
-    print(result)
